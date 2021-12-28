ccf2bbda49e2efe60d024a1ecc40df6c2ba20922
mhayashi-hub <redbrick.gmad@gmail.com>
2021-12-28 14:02:41 +0900

    2021/12/28 午後　pythonテスト

---
+++ pythontest/object/test01.py
@@ -0,0 +1,19 @@
+print(100 *5 + 40 *10)
+var = 1 + 1
+print(var)
+import calendar
+calendar.prmonth(2022,1)
+print(1,2,"A","漢字")
+def countup():
+    count
+    return count
+
+flag1 = "false"
+if flag1 == "true":
+    figure=input("文字列の入力をしてください ")
+    print("入力文字列 ", figure, " を出力します")
+elif flag1 == "false":
+    print("プログラムを完了します")
+    print(1>2)
+else:
+    print("プログラムを終了します")
